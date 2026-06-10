761e581fceecd1bd46071ad9faae1afbfb2a9873
czagoh <dorian.pennegat@hotmail.fr>
2026-06-10 10:27:26 +0200

    QuestReact - 05 Type

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,14 @@ const pokemonList = [
   },
   {
     name: "mew",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/151.png",
   },
 ];
 
 function App() {
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[0]} />
+      <PokemonCard pokemon={pokemonList[1]} />
     </div>
   );
 }
