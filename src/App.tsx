13f56f58653ccc34cede89eeceb57d016aa1aca6
czagoh <dorian.pennegat@hotmail.fr>
2026-06-11 12:08:41 +0200

    QuestReact - 06 State

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 
@@ -13,9 +14,19 @@ const pokemonList = [
 ];
 
 function App() {
+  const [pokemonName, setPokemonName] = useState("bulbasaur");
+
+  const pokemon = pokemonList.find((pokemon) => pokemon.name === pokemonName);
+
+  if (pokemon == null) {
+    throw new Error("Invalid pokemon name");
+  }
+
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[1]} />
+      <button onClick={() => setPokemonName("bulbasaur")}>bulbasaur</button>
+      <button onClick={() => setPokemonName("mew")}>mew</button>
+      <PokemonCard pokemon={pokemon} />
     </div>
   );
 }
