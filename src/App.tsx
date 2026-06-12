30e3751c2d35fa9b677a905793e2e5edb9723409
czagoh <dorian.pennegat@hotmail.fr>
2026-06-12 10:11:03 +0200

    QuestReact - 07 .map

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,28 @@ import PokemonCard from "./components/PokemonCard";
 
 const pokemonList = [
   {
-    name: "bulbasaur",
+    name: "Bulbasaur",
     imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
-    name: "mew",
-    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/151.png",
+    name: "Charmander",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "Squirtle",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "Pikachu",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "Mew",
   },
 ];
 
 function App() {
-  const [pokemonName, setPokemonName] = useState("bulbasaur");
+  const [pokemonName, setPokemonName] = useState("Bulbasaur");
 
   const pokemon = pokemonList.find((pokemon) => pokemon.name === pokemonName);
 
@@ -24,8 +35,12 @@ function App() {
 
   return (
     <div>
-      <button onClick={() => setPokemonName("bulbasaur")}>bulbasaur</button>
-      <button onClick={() => setPokemonName("mew")}>mew</button>
+      <nav>
+        {pokemonList.map((pokemon) => (
+          <button key={pokemon.name} onClick={() => setPokemonName(pokemon.name)}>{pokemon.name}
+          </button>
+        ))}
+      </nav>
       <PokemonCard pokemon={pokemon} />
     </div>
   );
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -9,11 +9,7 @@ function PokemonCard({ pokemon }: PokemonCardProps) {
 
   return (
     <figure>
-      {pokemon.imgSrc ? (
-        <img src={pokemon.imgSrc} alt={pokemon.name} />
-      ) : (
-        <p>???</p>
-      )}
+      {pokemon.imgSrc ? (<img src={pokemon.imgSrc} alt={pokemon.name} />) : (<p>???</p>)}
       <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
